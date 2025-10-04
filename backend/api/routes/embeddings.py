325e2b6d9e14c46b2eb2566cff0ebbf396149484
rlriederer7 <rlriederer7@gmail.com>
2025-10-03 21:45:39 -0400

    embedding routes - uploading documents endpoint which:
    1) chunks up documents
    2) generates embedding batches
    3) inserts chunks to database
    4) returns the assigned document id, provided name, number of chunks, and chunk ids
    
    semantic search endpoint which:
    1) generates an embedding of the user query
    2) searches for similar chunks up to the top_k limit
    3) returns the similar chunks as a list of tupled text and distance from the SearchRequest query

--- backend/api/routes/embeddings.py
+++ backend/api/routes/embeddings.py
@@ -1,7 +1,61 @@
-from fastapi import APIRouter
+import uuid
+from fastapi import APIRouter, HTTPException
+from backend.models.document import DocumentUpload, SearchRequest
+from backend.services.database import db_service
+from backend.services.embeddings import embedding_service, chunk_text
 
 router = APIRouter()
 
-@router.post("/encode")
-async def encode_text(text: str):
-    return {"This is not a real embedding": text}
+@router.post("/upload")
+async def upload_document(request: DocumentUpload):
+    try:
+        document_id = str(uuid.uuid4())
+        chunks = chunk_text(request.text)
+        if not chunks:
+            raise HTTPException(status_code=400, detail="Empty Document")
+
+        embeddings = await embedding_service.generate_embedding_batch(chunks)
+        chunk_ids = []
+        for idx, (chunk, embedding) in enumerate(zip(chunks, embeddings)):
+            chunk_id = await db_service.insert_chunk(
+                document_id = document_id,
+                chunk_index = idx,
+                text=chunk,
+                embedding = embedding
+            )
+            chunk_ids.append(chunk_id)
+        return {
+            "document_id":document_id,
+            "document_name":request.document_name,
+            "chunks_created": len(chunks),
+            "chunk_ids": chunk_ids,
+        }
+
+    except Exception as e:
+        print(f"Error during upload: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/search_semantic_only")
+async def search_documents(request: SearchRequest):
+    print(1)
+    try:
+        print(2)
+        query_embedding = await embedding_service.generate_embedding(request.query)
+        print(3)
+        similar_chunks = await db_service.search_similar(
+            query_embedding,
+            limit=request.top_k
+        )
+
+        if not similar_chunks:
+            raise HTTPException(
+                status_code=404,
+                detail="No documents found in database"
+            )
+
+        return similar_chunks
+
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
